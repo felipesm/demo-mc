8eb97e2623c6c54a82cd28304b03d0809ced7c93
Felipe Maia <felipe.silwa@gmail.com>
2019-10-27 13:50:17 -0300

    Criação projeto spring

---
+++ src/main/java/com/ifdeveloper/demomc/DemoMcApplication.java
@@ -0,0 +1,13 @@
+package com.ifdeveloper.demomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMcApplication.class, args);
+	}
+
+}

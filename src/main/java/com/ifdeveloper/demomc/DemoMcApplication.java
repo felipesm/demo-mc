0a8c21fae346c6751c0f96cdd8981680256d7b6f
Felipe Maia <felipe.silwa@gmail.com>
2019-11-02 17:02:23 -0300

    Criação de entidade Produto e fazendo associação com Categoria

--- src/main/java/com/ifdeveloper/demomc/DemoMcApplication.java
+++ src/main/java/com/ifdeveloper/demomc/DemoMcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ifdeveloper.demomc.domain.Categoria;
+import com.ifdeveloper.demomc.domain.Produto;
 import com.ifdeveloper.demomc.repositories.CategoriaRepository;
+import com.ifdeveloper.demomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class DemoMcApplication implements CommandLineRunner {
 	
 	@Autowired
-	private CategoriaRepository repositorio;
+	private CategoriaRepository categoriaRepositorio;
+	
+	@Autowired
+	private ProdutoRepository produtoRepositorio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMcApplication.class, args);
@@ -26,7 +31,19 @@ public class DemoMcApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 		
-		repositorio.saveAll(Arrays.asList(categoria1, categoria2));
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 600.00);
+		Produto produto3 = new Produto(null, "Monitor", 800.00);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+		
+		categoriaRepositorio.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepositorio.saveAll(Arrays.asList(produto1, produto2, produto3));
 		
 	}
 

5c0495e36a1675bb80ba66f1a1735facdb3b41b4
Felipe Maia <felipe.silwa@gmail.com>
2019-12-07 18:31:20 -0300

    Personalizando mensagens de validação

---
+++ src/main/java/com/ifdeveloper/demomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package com.ifdeveloper.demomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> listaErros = new ArrayList<>();
+
+	public ValidationError(Integer status, String mensagem, Long horaExcecao) {
+		super(status, mensagem, horaExcecao);
+	}
+
+	public List<FieldMessage> getListaDeErros() {
+		return listaErros;
+	}
+	
+	public void adicionarErro(String nomeCampo, String mensagem) {
+		listaErros.add(new FieldMessage(nomeCampo, mensagem));
+	}
+
+}

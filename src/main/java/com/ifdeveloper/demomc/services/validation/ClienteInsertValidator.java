a468320c061242010c34b58722e46cc0d69ce931
Felipe Maia <felipe.silwa@gmail.com>
2019-12-22 13:49:22 -0300

    Adição de validação para Email

--- src/main/java/com/ifdeveloper/demomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/ifdeveloper/demomc/services/validation/ClienteInsertValidator.java
@@ -2,15 +2,23 @@ package com.ifdeveloper.demomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ifdeveloper.demomc.domain.Cliente;
 import com.ifdeveloper.demomc.domain.enums.TipoCliente;
 import com.ifdeveloper.demomc.dto.NovoClienteDTO;
+import com.ifdeveloper.demomc.repositories.ClienteRepository;
 import com.ifdeveloper.demomc.resources.exceptions.FieldMessage;
 import com.ifdeveloper.demomc.services.validation.utils.ValidaCpfCNPJ;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, NovoClienteDTO> {
+	
+	@Autowired
+	private ClienteRepository repositorio;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !ValidaCpfCNPJ.validaCNPJ(objDto.getNumeroInscricao())) {
 			lista.add(new FieldMessage("numeroInscricao", "CNPJ Inválido"));
+		}
+		
+		Cliente cliente = repositorio.findByEmail(objDto.getEmail());
+		if (cliente != null) {
+			lista.add(new FieldMessage("email", "Email já cadastrado"));
 		}
 
 		for (FieldMessage e : lista) {
